b2b7682af560728bfc0c684475fd672222c76041
Sumit Sharma <sumit.sharma@radialcode.com>
2023-01-01 03:45:57 +0530

    jwt via cookie & no pass sent from server

--- controllers/authCotroller.js
+++ controllers/authCotroller.js
@@ -30,6 +30,7 @@ exports.signup = catchAsync(async (req, res, next) => {
    * Creating JWT tokens
    */
   const token = signToken(newUser._id);
+  newUser.password = undefined;
 
   res.status(201).json({
     status: "success",
@@ -54,6 +55,12 @@ exports.login = catchAsync(async (req, res, next) => {
   }
   // 2) If everything is ok, send token to client
   const token = signToken(user._id);
+  // secure makes sure the cookie is only sent in https Secure connection
+  res.cookie("jwt", token, {
+    expires: new Date(Date.now + 90 * 24 * 60 * 1000),
+    // secure: true,
+    httpOnly: true,
+  });
 
   res.status(200).json({
     status: "success",
@@ -75,7 +82,11 @@ exports.protect = catchAsync(async (req, res, next) => {
   // 2) Validate the token
   const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
   console.log({ decoded });
+
   // 3) Check if the user exists
+  const userFound = await User.findById(decoded.id);
+  if (!userFound) next();
   // 4) Check if user changed password after the token is issued
+  // This can be done by simply comparing the timestamps of password changed at & decoded.iat
   next();
 });
